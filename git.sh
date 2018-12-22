ec68f074358e9d721b0700085ccf6a1047888dc6
Namean <dwilsoncnc@gmail.com>
2018-12-22 15:49:48 +0000

    adding files from tutorial

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# chrome-extension" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Namean/chrome-extension.git
+git push -u origin master
